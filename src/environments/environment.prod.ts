4eb35df5d15261f1bdb31f7dcec01ac0285ea04a
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-06-09 18:22:52 +0100

    Production setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,22 @@
 export const environment = {
-  production: true
+  production: true,
+
+  BASE_URL: 'https://jsonplaceholder.typicode.com',
+  REQUESTS: [
+    {
+      verb: 'get',
+      title: '/todos/1',
+      url: 'https://jsonplaceholder.typicode.com/todos/1',
+    },
+    {
+      verb: 'get',
+      title: '/todos/1',
+      url: 'https://jsonplaceholder.typicode.com/todos/1',
+    },
+    {
+      verb: 'get',
+      title: '/todos/1',
+      url: 'https://jsonplaceholder.typicode.com/todos/1',
+    },
+  ],
 };
