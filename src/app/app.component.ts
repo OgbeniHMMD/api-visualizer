63d427302746286f9dddb7d9a071e81f4f1453d9
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-06-09 17:42:23 +0100

    Basic prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'api-visualizer';
+
+  result = [
+    { userId: 1, id: 1, title: 'delectus aut autem', completed: false },
+    { userId: 1, id: 1, title: 'delectus aut autem', completed: false },
+    { userId: 1, id: 1, title: 'delectus aut autem', completed: false },
+    { userId: 1, id: 1, title: 'delectus aut autem', completed: false },
+  ];
+
+  formatCode(v: object) {
+    return JSON.stringify(v, null, 4);
+  }
 }
